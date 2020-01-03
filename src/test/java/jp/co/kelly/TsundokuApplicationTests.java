64e5dc618c039606aef972d07e790bc2716fdcfd
nainaistar <nainaistar@gmail.com>
2020-01-03 18:42:19 +0900

    [Refactor] rename

--- src/test/java/jp/co/kelly/TsundokuApplicationTests.java
+++ src/test/java/jp/co/kelly/TsundokuApplicationTests.java
@@ -1,10 +1,10 @@
-package jp.co.kelly.bookReadLimit;
+package jp.co.kelly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookReadLimitApplicationTests {
+class TsundokuApplicationTests {
 
 	@Test
 	void contextLoads() {
