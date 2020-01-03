56f8a0ef1f974f6233ec78ac6928b15cee71010b
nainaistar <nainaistar@gmail.com>
2020-01-03 18:37:19 +0900

    [Add] first commit

---
+++ src/main/java/jp/co/kelly/bookReadLimit/BookReadLimitApplication.java
@@ -0,0 +1,13 @@
+package jp.co.kelly.bookReadLimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReadLimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReadLimitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/kelly/bookReadLimit/BookReadLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.kelly.bookReadLimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReadLimitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
